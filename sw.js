69912857c366f8f05320e4a055541d9a0259b0ff
iamteerawut <t.kitbunjerdjarud@gmail.com>
2019-04-08 16:05:49 +0700

    Update font

--- sw.js
+++ sw.js
@@ -2,24 +2,24 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/2330b863ccae487d1de8.js",
-    "revision": "34163d0a9c3fa90721b941dc13d127ea"
+    "url": "/_nuxt/0707ca3fe5a4d1ca3e75.js",
+    "revision": "ef822a22f36888c5e18832558a630282"
   },
   {
-    "url": "/_nuxt/2ed24c099ea2d06f42ca.js",
-    "revision": "13a8ca23ea9f29f52541f9d75f892d73"
+    "url": "/_nuxt/2330b863ccae487d1de8.js",
+    "revision": "34163d0a9c3fa90721b941dc13d127ea"
   },
   {
     "url": "/_nuxt/364a06f2a7b0095a6fdc.js",
     "revision": "c6ee3acf076b1e66e267df7ff95986ba"
   },
   {
-    "url": "/_nuxt/9c558c101f14474997d4.js",
-    "revision": "25ddeaedeead1a48664c459066a1fd7f"
+    "url": "/_nuxt/6691d3b4aa67d670d56e.js",
+    "revision": "4a121963894045c8694b63b277d16dfd"
   },
   {
-    "url": "/_nuxt/b29eaae45d913bf0c3eb.js",
-    "revision": "39959e42047ffa1e24ec195c1dba35d7"
+    "url": "/_nuxt/cdd8d0cabda05e6c8501.js",
+    "revision": "bb85267c08bdb9a1e607f96df2e2b84c"
   },
   {
     "url": "/_nuxt/e07325b57bbda6a00f1d.js",
